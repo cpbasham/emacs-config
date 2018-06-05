04b602f9e6b8999fe95f06375956cc3e19d03bae
Cameron Preston Basham <cameron.basham@gmail.com>
2018-06-05 11:48:56 -0600

    initial commit

---
+++ init.el
@@ -0,0 +1,32 @@
+
+;; Added by Package.el.
+(package-initialize)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(custom-enabled-themes (quote (wheatgrass)))
+ '(inhibit-startup-buffer-menu t)
+ '(inhibit-startup-screen t)
+ '(initial-scratch-message "*scratch*
+
+")
+ '(package-archives
+   (quote
+    (("gnu" . "https://elpa.gnu.org/packages/")
+     ("melpa" . "https://melpa.org/packages/"))))
+ '(package-selected-packages (quote (discover))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(setq mac-right-command-modifier 'meta)
